1c00120acb10f08203c9d7c0bb4d4bd354b3423b
Mohamed Elshamy <93993421+devM7MD@users.noreply.github.com>
2025-04-02 23:50:14 +0400

    Update models.py

--- dcrm/models.py
+++ dcrm/models.py
@@ -2,12 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    username = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=128)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, unique=True)
-    is_admin = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.username
