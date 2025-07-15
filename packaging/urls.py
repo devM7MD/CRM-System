9ac73188788efa806ccd49c4ce1e4b6d286c11ea
YassenDev <91493974+YassenDev@users.noreply.github.com>
2025-07-15 23:02:34 +0300

    Delete All Project

--- packaging/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'packaging'
-
-urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('orders/', views.order_list, name='orders'),
-] 
--- subscribers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'subscribers'
-
-urlpatterns = [
-    path('', views.subscribers_list, name='list'),
-    path('add/', views.add_user, name='add_user'),
-] 
